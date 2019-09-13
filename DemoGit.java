560ddfa863cfdd3ebbb9570b0e2196c438618ed4
Brendan Marks <brendan99@live.ca>
2019-09-13 13:08:48 -0400

    first commit

---
+++ DemoGit.java
@@ -0,0 +1,8 @@
+public class DemoGit {
+	public static void main(String[] args){
+	System.out.println("Hello");
+	}
+	public static int divider(int numerator, int denomenator){
+		return (numerator / denomenator);
+	}
+}
